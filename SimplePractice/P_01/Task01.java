fb32ef4453f7f6581288ee77412c63231ecb06da
ValeriyV <valeriy.vasipov@gmail.com>
2022-09-16 21:14:57 +0300

    task1

---
+++ SimplePractice/P_01/Task01.java
@@ -0,0 +1,10 @@
+public class Task01 {
+    public static void main(String[] args) {
+        int n = 10, i = 0;
+        String java = "JAVA";
+        while(i < n){
+            System.out.print(java + " ");
+            i++;
+        }
+    }
+}
