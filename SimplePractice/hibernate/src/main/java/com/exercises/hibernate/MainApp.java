97946362bee0baac02025149a111e1e7116a6b0f
Tirsh <valeriy.vasipov@gmail.com>
2022-10-04 19:10:12 +0300

    Hibernate_tests

--- SimplePractice/hibernate/src/main/java/com/exercises/hibernate/MainApp.java
+++ SimplePractice/hibernate/src/main/java/com/exercises/hibernate/MainApp.java
@@ -4,12 +4,56 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class MainApp {
     public static void main(String[] args) {
         SessionFactory sessionFactory = new Configuration()
+                .addAnnotatedClass(Item.class)
                 .buildSessionFactory();
+        Session session = null;
+
+        // =====CREATE=====
+//        session = sessionFactory.getCurrentSession();
+//        session.beginTransaction();
+//        Item milk = new Item("Milk", 80);
+//        System.out.println(milk);
+//        session.save(milk);
+//        System.out.println(milk);
+//        session.getTransaction().commit();
+        // =====GET FROM DB=====
+//        session = sessionFactory.getCurrentSession();
+//        session.beginTransaction();
+//        Item itemFromDb = session.get(Item.class, 1L);
+//        session.getTransaction().commit();
+//        System.out.println(itemFromDb);
+
+        session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+//        Item newItem = session.createQuery("SELECT i FROM Item i WHERE i.id = 2", Item.class)
+////                .getResultList();
+//                .getSingleResult();
+
+        List<Item> newItems = session.createQuery("SELECT i FROM Item i", Item.class)
+                .getResultList();
+        System.out.println(newItems);
+        session.getTransaction().commit();
+
+        // =====UPDATE=====
+//        session = sessionFactory.getCurrentSession();
+//        session.beginTransaction();
+//        Item itemFromDb = session.get(Item.class, 1L);
+//        itemFromDb.setPrice(200);
+//        //session.flush();
+//        session.getTransaction().commit();
+        // =====DELETE=====
+//        session = sessionFactory.getCurrentSession();
+//        session.beginTransaction();
+//        Item itemFromDb = session.get(Item.class, 1L);
+//        session.remove(itemFromDb);
+//        session.getTransaction().commit();
+
 
         sessionFactory.close();
-
     }
 }
