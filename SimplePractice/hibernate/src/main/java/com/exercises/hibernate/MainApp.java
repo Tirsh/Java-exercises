8fc28f498f3ca4ece01f617d0d66f531d28748d4
Tirsh <valeriy.vasipov@gmail.com>
2022-10-04 12:19:40 +0300

    hibernate_tests

--- SimplePractice/hibernate/src/main/java/com/exercises/hibernate/MainApp.java
+++ SimplePractice/hibernate/src/main/java/com/exercises/hibernate/MainApp.java
@@ -1,7 +1,15 @@
 package com.exercises.hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class MainApp {
     public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .buildSessionFactory();
+
+        sessionFactory.close();
 
     }
 }
