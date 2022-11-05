5e8e2f0e8d15b5f457f829201d636eb186901c97
Tirsh <valeriy.vasipov@gmail.com>
2022-11-05 22:52:06 +0300

    Spring_context

--- Spring/src/main/java/com/tirsh/ClassicMusic.java
+++ Spring/src/main/java/com/tirsh/ClassicMusic.java
@@ -1,6 +1,12 @@
 package com.tirsh;
 
 public class ClassicMusic implements Music{
+    private void makeInit(){
+        System.out.println("Make initialize");
+    }
+    private  void makeDestroy(){
+        System.out.println("Make destruction");
+    }
     @Override
     public String getSong() {
         return "Moon light sonate";
--- Spring/src/main/java/com/tirsh/RapMusic.java
+++ Spring/src/main/java/com/tirsh/RapMusic.java
@@ -1,6 +1,10 @@
 package com.tirsh;
 
 public class RapMusic implements Music{
+    private RapMusic(){}
+    public static RapMusic getRapMusic(){
+        return new RapMusic();
+    }
     @Override
     public String getSong() {
         return "Drop it, like it's hot";
